c79fd917a4f332f672f6aac91369dcb2b37e62f9
Haripeak <yuvarajdm96gmail.com>
2025-02-21 12:48:16 +0530

    Added Js

---
+++ product.js
@@ -0,0 +1,35 @@
+//Selecting side Navbar, Menu
+
+var sidenav = document.getElementById("sidenav")
+
+var menuicon = document.getElementById("menuicon")
+
+var closenav = document.getElementById("closenav")
+
+menuicon.addEventListener("click", function () {
+    sidenav.style.right = 0
+})
+closenav.addEventListener("click", function () {
+    sidenav.style.right = "-100%"
+})
+
+//Product Search Function
+var productContainer = document.getElementById("product-container")
+var hari = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+hari.addEventListener("keyup", function () {
+    var enter = event.target.value.toUpperCase()
+
+    for (count = 0; count < productlist.length; count = count + 1) {
+        var productname = productlist[count].querySelector("h1").textContent
+        if (productname.toUpperCase().indexOf(enter) < 0) {
+            productlist[count].style.display = "none"
+        }
+        else {
+            productlist[count].style.display = "block"
+
+        }
+    }
+})
+
